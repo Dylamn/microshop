37ea55ff75b9d03a145bff48a094f980f32b5bf8
Dylamn <dylan.vallee@hotmail.com>
2025-10-28 18:27:25 +0100

    feat(log): enhance JsonFormatter with orjson and Pydantic model support
    - Replace `json` with `orjson` for better performance and serialization
    - Add support for serializing Pydantic models via `model_dump`

--- userservice/app/core/log/formatters/json_formatter.py
+++ userservice/app/core/log/formatters/json_formatter.py
@@ -1,7 +1,9 @@
-import json
 import logging
 from datetime import UTC, datetime
 from typing import Any
+
+import orjson
+from pydantic import BaseModel
 
 
 class JsonFormatter(logging.Formatter):
@@ -17,10 +19,12 @@ class JsonFormatter(logging.Formatter):
 
         # Add extra fields if available
         for key, value in record.__dict__.items():
+            if isinstance(value, BaseModel):
+                value = value.model_dump()
             if key not in ("args", "msg", "exc_info", "exc_text", "stack_info"):
                 log_entry[key] = value
 
         if record.exc_info:
             log_entry["exception"] = self.formatException(record.exc_info)
 
-        return json.dumps(log_entry)
+        return orjson.dumps(log_entry).decode("utf-8")
