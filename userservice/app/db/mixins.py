882c484bf9aae585b8fcaee2b01eb28bb25ddef6
Dylamn <dylan.vallee@hotmail.com>
2025-11-06 22:51:25 +0100

    refactor(timestamps): change from 'func.now()' to `CURRENT_TIMESTAMP`
    Switching to a default value supported by sqlite

--- userservice/app/db/mixins.py
+++ userservice/app/db/mixins.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime, func
+from sqlalchemy import DateTime, func, text
 from sqlalchemy.orm import Mapped, mapped_column
 
 
@@ -19,5 +19,5 @@ class TimestampMixin:
         updated_at (Mapped[datetime]): The timestamp marking the last update of the
             record. It is updated each time the record is modified.
     """
-    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
-    updated_at: Mapped[datetime] = mapped_column(DateTime, onupdate=func.now(), nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=text("CURRENT_TIMESTAMP"))
+    updated_at: Mapped[datetime] = mapped_column(DateTime, onupdate=text("CURRENT_TIMESTAMP"), nullable=True)
--- userservice/migrations/versions/2025_10_27_1646-d134f6539130_create_users_table.py
+++ userservice/migrations/versions/2025_10_27_1646-d134f6539130_create_users_table.py
@@ -26,7 +26,7 @@ def upgrade() -> None:
     sa.Column('username', sa.String(length=30), nullable=False),
     sa.Column('email', sa.String(length=120), nullable=False),
     sa.Column('password', sa.String(length=100), nullable=True),
-    sa.Column('created_at', sa.DateTime(), server_default=sa.text('now()'), nullable=False),
+    sa.Column('created_at', sa.DateTime(), server_default=sa.text('CURRENT_TIMESTAMP'), nullable=False),
     sa.Column('updated_at', sa.DateTime(), nullable=True),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('email')
